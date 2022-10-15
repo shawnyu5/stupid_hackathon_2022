258289556a6ccd9e20af2bf115f9adb09bdafed0
shawn yu <shawn.yu5@hotmail.com>
2022-10-15 12:30:21 -0400

    feat: able to capture output from device

--- audio_processing/audio.py
+++ audio_processing/audio.py
@@ -15,22 +15,22 @@ info = audio.get_host_api_info_by_index(0)
 numdevices = info.get("deviceCount")
 
 
-for ii in range(audio.get_device_count()):
-    print(audio.get_device_info_by_index(ii).get("name"))
-# for i in range(0, numdevices):
-    # if (
-        # audio.get_device_info_by_host_api_device_index(0, i).get("maxInputChannels")
-    # ) > 0:
-        # print(
-            # "Input Device id ",
-            # i,
-            # " - ",
-            # audio.get_device_info_by_host_api_device_index(0, i).get("name"),
-        # )
+# for ii in range(audio.get_device_count()):
+    # print(audio.get_device_info_by_index(ii).get("name"))
+for i in range(0, numdevices):
+    if (
+        audio.get_device_info_by_host_api_device_index(0, i).get("maxInputChannels")
+    ) > 0:
+        print(
+            "Input Device id ",
+            i,
+            " - ",
+            audio.get_device_info_by_host_api_device_index(0, i).get("name"),
+        )
 
 print("-------------------------------------------------------------")
 
-exit(0)
+# exit(0)
 
 index = int(input())
 print("recording via index " + str(index))
